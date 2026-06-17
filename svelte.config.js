47a574bbe3007f93131ddd7bf8a33b70fad13d7d
badlandschugs <robloxfan2017@icloud.com>
2026-06-16 22:02:17 -0400

    no more static rendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,9 +8,7 @@ const config = {
   },
 
   kit: {
-    adapter: adapter({
-      fallback: "index.html",
-    }),
+    adapter: adapter(),
   },
 };
 
