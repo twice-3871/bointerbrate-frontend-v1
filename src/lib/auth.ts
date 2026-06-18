9bea852efb5862ad54885ab258c2ce9573f3d583
badlandschugs <robloxfan2017@icloud.com>
2026-06-18 13:32:19 -0400

    fixing auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,24 +3,31 @@ import { writable } from "svelte/store";
 import { api } from "./api";
 
 export const user = writable<User | null>(null)
+let authInitialized = false;
 
 export async function initAuth() {
+    if (authInitialized) return
+
+    console.log("INIT AUTH RUN");
+     if (!browser) return;
+
     const token = localStorage.getItem("token");
-    if (!token) {
-        user.set(null)
-        return
-    }
+    
+    if (!token) return;
+    console.log("TOKEN:", token);
+
 
     try {
-        const me = await api<User>("auth/me")
-        console.log("me: ", me)
-        user.set(me)
+        const me = await api<User>("auth/me");
+        console.log("AUTH SUCCESS:", me);
+        user.set(me);
     } catch (e) {
-        console.log(e)
-        localStorage.removeItem("token")
-        user.set(null)
+        console.log("AUTH FAILED:", e);
+        localStorage.removeItem("token");
+        user.set(null);
     }
-    
+
+    authInitialized = true
 }
 
 export function logout() {
