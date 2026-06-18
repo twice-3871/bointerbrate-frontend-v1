888c4d9cb052601d3504f22dccead60091b87a61
badlandschugs <robloxfan2017@icloud.com>
2026-06-18 14:22:29 -0400

    fixing mobile ui

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   let open = $state(false);
+  import { user } from "$lib/auth";
+  const API_URL = import.meta.env.VITE_API_URL;
 </script>
 
 <nav class="bg-blue-600 text-white px-4 py-3">
@@ -10,8 +12,17 @@
     <!-- Desktop links -->
     <div class="hidden md:flex gap-4">
       <a href="/">Home</a>
-      <a href="/levels">Levels</a>
+      <a href="/list/classic">List</a>
       <a href="/submit">Submit</a>
+      {#if $user}
+        <img
+          src={`https://cdn.discordapp.com/avatars/${$user.id}/${$user.avatar}.png`}
+          alt="avatar"
+          class="w-15 h-15 rounded-full"
+        />
+      {:else}
+        <a href={`${API_URL}/auth/login`}>Log In</a>
+      {/if}
     </div>
 
     <!-- Mobile button -->
@@ -22,8 +33,19 @@
   {#if open}
     <div class="flex flex-col gap-3 mt-3 md:hidden">
       <a href="/" onclick={() => (open = false)}>Home</a>
-      <a href="/levels" onclick={() => (open = false)}>Levels</a>
+      <a href="/list/classic" onclick={() => (open = false)}>List</a>
       <a href="/submit" onclick={() => (open = false)}>Submit</a>
+      {#if $user}
+        <img
+          src={`https://cdn.discordapp.com/avatars/${$user.id}/${$user.avatar}.png`}
+          alt="avatar"
+          class="w-15 h-15 rounded-full"
+        />
+      {:else}
+        <a href={`${API_URL}/auth/login`} onclick={() => (open = false)}
+          >Log In</a
+        >
+      {/if}
     </div>
   {/if}
 </nav>
--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -1,5 +1,6 @@
 type User = {
     id: string,
     username: string,
+    avatar: string,
     is_allowed: boolean
 }
